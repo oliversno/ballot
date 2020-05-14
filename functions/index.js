a12c283274305efebfb5b0fed13554c144393ede
Sean Oliver <snoliver@umich.edu>
2020-05-14 13:04:50 -0400

    problem: deletes instantly

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,13 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
 // run `firebase deploy` to  sync with firebase
-exports.deleteOldItems = functions.database.ref('/ballot')
-.onWrite((change, context) => {
+exports.deleteOldItems = functions.database.ref('/ballots')
+.onWrite((change, context) => { //onWrite because having a schedualed job costs $0.10 a month
   var ref = change.after.ref.parent; // reference to the items
   var now = Date.now();
-  var cutoff = now - 5 * 1000; //older than 5 seconds
+  var cutoff = now - 0.5 * 60 * 60 * 1000; //older than 1/2 hour
   var oldItemsQuery = ref.orderByChild('timeLastUpdated').endAt(cutoff);
   return oldItemsQuery.once('value', function(snapshot) {
     // create a map with all children that need to be removed
