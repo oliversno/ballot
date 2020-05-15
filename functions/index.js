514e155aad8e4471b0ec6fc9dcaa0b9dfafc9268
Sean Oliver <snoliver@umich.edu>
2020-05-15 16:21:35 -0400

    still failing

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,17 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 // run `firebase deploy` to  sync with firebase
-exports.deleteOldItems = functions.database.ref('/ballots')
-.onWrite((change, context) => { //onWrite because having a schedualed job costs $0.10 a month
-  var ref = change.after.ref.parent; // reference to the items
+exports.deleteOldItems = functions.database.ref('/ballots/')
+.onUpdate((change, context) => { //onUpdate because having a schedualed job costs $0.10 a month
   var now = Date.now();
   var cutoff = now - 7 * 24 * 60 * 60 * 1000; //ballot hasn't been voted on in one week
-  var oldItemsQuery = ref.orderByChild('timeLastUpdated').endAt(cutoff);
-  return oldItemsQuery.on('child_added', function(data) {
-    // create a map with all children that need to be removed
-    var updates = {};
-    updates[data.key] = null
-    // execute all updates in one go and return the result to end the function
-    return ref.update(updates);
+  var ballotRef = change.before.ref;
+  console.log("Ballot ref is", ballotRef.toString());
+  var query = ballotRef.orderByChild('timeLastUpdated');
+  console.log("query is", query.toString());
+  return query.once('value').then(function(data){
+    console.log("key is", data.key);
+    return;
   });
 });
 
